fad607e76413ed756cd200f246311830fd6c1131
Hugo Cardoso <ha.cardoso30@gmail.com>
2022-03-26 18:21:57 +0000

    finito por hoje

--- backend/grammars/json_schema/refs.js
+++ backend/grammars/json_schema/refs.js
@@ -16,7 +16,7 @@ function resolve_refs(data, settings) {
 			if (!(/^anon\d+/.test(subschema.id) && !subschema.refs.length)) refs.push(subschema)
 		}
 	}
-	
+
 	let crossRefs = resolve_foreignRefs(refs)
 	if (crossRefs !== true) return crossRefs
 	return true
@@ -47,6 +47,68 @@ function resolve_localRefs(json, schema_id, schema_refs, recursiv) {
 	}
 	
 	return true
+}
+  
+function resolve_foreignRefs(refs) {
+    let refs_map = refs.reduce((acc, cur) => {acc[cur.id] = cur.refs.map(x => x.$ref); return acc}, {})
+    
+    for (let k in refs_map) {
+      for (let i = 0; i < refs_map[k].length; i++) {
+        let ref = refs_map[k][i]
+  
+        // loop infinito de recursividade
+        if (ref in refs_map && refs_map[ref].includes(k)) return `Existe um ciclo infinito de recursividade entre as schemas '${k}' e '${ref}'!`
+      }
+    }
+  
+    let ids = Object.keys(refs_map)
+    let queue = ids.filter(k => !refs_map[k].length)
+  
+    while (queue.length !== ids.length) {
+      Object.keys(refs_map).filter(k => !queue.includes(k)).map(id => {
+        let parsedIndexes = []
+  
+        for (let i = 0; i < refs_map[id].length; i++) {
+          let ref = refs_map[id][i], schema, nested_ref = false
+  
+          if (queue.includes(ref)) {
+            let ref_id = queue[queue.findIndex(x => ref.startsWith(x))]
+  
+            schema = replace_ref(ref.replace(ref_id, "#").split("/"), refs[refs.findIndex(x => x.id == ref)].schema)
+            if (schema === false) return `A $ref '${refs_map[id][i]}' é inválida!`
+            if (schema !== true && "$ref" in schema) nested_ref = true
+  
+            let refs_elem = refs[refs.findIndex(x => x.id == id)]
+            delete refs_elem.refs[i].$ref
+            Object.assign(refs_elem.refs[i], schema)
+  
+            if (nested_ref) i--
+            else parsedIndexes.push(i) 
+          }
+        }
+  
+        parsedIndexes.map(i => refs_map[id].splice(i, 1))
+        if (!refs_map[id].length) queue.push(id)
+      })
+    }
+  
+    return true
+}
+  
+function replace_ref(ref, json) {
+	console.log("replace ref")
+	for (let i = 1; i < ref.length; i++) {
+		if (ref[i] in json) json = json[ref[i]]
+		else if ("type" in json) {
+			if ("object" in json.type && ref[i] in json.type.object) json = json.type.object[ref[i]]
+			else if ("array" in json.type && ref[i] in json.type.array) json = json.type.array[ref[i]]
+			else return false
+		}
+		else return false
+	}
+
+	if (typeof json == "boolean" || typeof json === 'object' && !Array.isArray(json) && json !== null) return json
+	return false
 }
 
 function resolve_recursiveRefs(json, schema_id, schema_ref, recursiv) {
@@ -126,67 +188,6 @@ function get_refPath(json, schema_id, path, depth) {
 		}
 	}
 
-	return false
-}
-  
-function resolve_foreignRefs(refs) {
-    let refs_map = refs.reduce((acc, cur) => {acc[cur.id] = cur.refs.map(x => x.$ref); return acc}, {})
-    
-    for (let k in refs_map) {
-      for (let i = 0; i < refs_map[k].length; i++) {
-        let ref = refs_map[k][i]
-  
-        // loop infinito de recursividade
-        if (ref in refs_map && refs_map[ref].includes(k)) return `Existe um ciclo infinito de recursividade entre as schemas '${k}' e '${ref}'!`
-      }
-    }
-  
-    let ids = Object.keys(refs_map)
-    let queue = ids.filter(k => !refs_map[k].length)
-  
-    while (queue.length !== ids.length) {
-      Object.keys(refs_map).filter(k => !queue.includes(k)).map(id => {
-        let parsedIndexes = []
-  
-        for (let i = 0; i < refs_map[id].length; i++) {
-          let ref = refs_map[id][i], schema, nested_ref = false
-  
-          if (queue.includes(ref)) {
-            let ref_id = queue[queue.findIndex(x => ref.startsWith(x))]
-  
-            schema = replace_ref(ref.replace(ref_id, "#").split("/"), refs[refs.findIndex(x => x.id == ref)].schema)
-            if (schema === false) return `A $ref '${refs_map[id][i]}' é inválida!`
-            if (schema !== true && "$ref" in schema) nested_ref = true
-  
-            let refs_elem = refs[refs.findIndex(x => x.id == id)]
-            delete refs_elem.refs[i].$ref
-            Object.assign(refs_elem.refs[i], schema)
-  
-            if (nested_ref) i--
-            else parsedIndexes.push(i) 
-          }
-        }
-  
-        parsedIndexes.map(i => refs_map[id].splice(i, 1))
-        if (!refs_map[id].length) queue.push(id)
-      })
-    }
-  
-    return true
-}
-  
-function replace_ref(ref, json) {
-	for (let i = 1; i < ref.length; i++) {
-		if (ref[i] in json) json = json[ref[i]]
-		else if ("type" in json) {
-			if ("object" in json.type && ref[i] in json.type.object) json = json.type.object[ref[i]]
-			else if ("array" in json.type && ref[i] in json.type.array) json = json.type.array[ref[i]]
-			else return false
-		}
-		else return false
-	}
-
-	if (typeof json == "boolean" || typeof json === 'object' && !Array.isArray(json) && json !== null) return json
 	return false
 }
 
